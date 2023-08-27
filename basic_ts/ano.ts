b07509cab1beca3c4d7e284f402c402de4525326
root <root@tehyoyee.localdomain>
2023-08-27 21:19:39 +0900

    add ano.ts

--- basic_ts/ano.ts
+++ basic_ts/ano.ts
@@ -1,1 +1,13 @@
-console.log("hello world!");
+// 타입 주석
+const a: number = 3;
+const b: string = "hi ts";
+const c: boolean = true;
+
+// any - 다들어감 - 피하기
+// number - int Long(big int) smallint
+// string - "" '' ``
+// boolean - true false
+// object - {} any 안쓴다.
+
+// 타입 추론
+let d = 1;
